0f95c340c6a2509aadfee6739d327ed9c5cac0eb
自然醒 <2507983704@qq.com>
2023-02-19 23:32:32 +0800

    feat(project): delete the pnpm config

--- src/pages/Admin/Common/Info.jsx
+++ src/pages/Admin/Common/Info.jsx
@@ -1,4 +1,3 @@
-
 import { ProTable } from '@ant-design/pro-components';
 import { Button, message, Popconfirm, Space, Tag } from 'antd';
 import { EllipsisOutlined, PlusOutlined } from '@ant-design/icons';
@@ -10,7 +9,7 @@ import {
 	ProFormDateRangePicker,
 	ProFormSelect,
 	ProFormText,
-  } from '@ant-design/pro-components';
+} from '@ant-design/pro-components';
 
 const Info = () => {
 	const [ messageApi, contextHolder ] = message.useMessage();
@@ -61,35 +60,35 @@ const Info = () => {
 						删除
 					</a>
 				</Popconfirm>,
-				<ModalForm key='update'
-					title="更新普通用户"
-					trigger={
-						<a key="update">更新</a>
-					}
-					submitTimeout={2000}
-					onFinish={async (values) => {
-						const { id,name,email,password } = values;
-						await personApi.updateCommon({ id,name,email,password });
-						message.success('更新成功');
-						return true;
-					}}			
+				<ModalForm key="update"
+									 title="更新普通用户"
+									 trigger={
+										 <a key="update">更新</a>
+									 }
+									 submitTimeout={ 2000 }
+									 onFinish={ async (values) => {
+										 const { id, name, email, password } = values;
+										 await personApi.updateCommon({ id, name, email, password });
+										 message.success('更新成功');
+										 return true;
+									 } }
 				>
 					<ProForm.Group>
-						<ProFormText width="md" name="id" disabled label="编号" initialValue={`${ id }`}/>
+						<ProFormText width="md" name="id" disabled label="编号" initialValue={ `${ id }` }/>
 					</ProForm.Group>
 					<ProForm.Group>
 						<ProFormText
-						width="md"
-						name="name"
-						label="用户名"
-						placeholder="请输入用户名"
+							width="md"
+							name="name"
+							label="用户名"
+							placeholder="请输入用户名"
 						/>
 					</ProForm.Group>
 					<ProForm.Group>
-						<ProFormText width="md" name="email" label="邮箱" placeholder="请输入邮箱" />
+						<ProFormText width="md" name="email" label="邮箱" placeholder="请输入邮箱"/>
 					</ProForm.Group>
 					<ProForm.Group>
-						<ProFormText.Password width="md" name="password" label="密码" type="password" placeholder="请输入密码" />
+						<ProFormText.Password width="md" name="password" label="密码" type="password" placeholder="请输入密码"/>
 					</ProForm.Group>
 				</ModalForm>,
 			],
@@ -100,9 +99,9 @@ const Info = () => {
 			<ProTable
 				cardBordered
 				columns={ columns }
-				request = { async (params = {})=>{
-					const { data:{ code,result } }=await personApi.searchCommonInfo(params.id);
-					if(code !== 200){
+				request={ async (params = {}) => {
+					const { data: { code, result } } = await personApi.searchCommonInfo(params.id);
+					if (code !== 200) {
 						messageApi.open({
 							type: 'error',
 							content: '搜索失败',
@@ -118,42 +117,42 @@ const Info = () => {
 						data: ret,
 						success: code === 200,
 					};
-				}}
+				} }
 				rowKey="id"
 				headerTitle="普通用户列表"
-				toolBarRender={() => [
-					<ModalForm key='insert'
-						title="添加普通用户"
-						trigger={
-							<Button type="primary">
-							<PlusOutlined />
-							添加
-							</Button>
-						}
-						submitTimeout={2000}
-						onFinish={async (values) => {
-							const { name,email,password } = values;
-							await personApi.insertCommon({ name,email,password });
-							message.success('提交成功');
-							return true;
-						}}			
+				toolBarRender={ () => [
+					<ModalForm key="insert"
+										 title="添加普通用户"
+										 trigger={
+											 <Button type="primary">
+												 <PlusOutlined/>
+												 添加
+											 </Button>
+										 }
+										 submitTimeout={ 2000 }
+										 onFinish={ async (values) => {
+											 const { name, email, password } = values;
+											 await personApi.insertCommon({ name, email, password });
+											 message.success('提交成功');
+											 return true;
+										 } }
 					>
-						 <ProForm.Group>
+						<ProForm.Group>
 							<ProFormText
-							width="md"
-							name="name"
-							label="用户名"
-							placeholder="请输入用户名"
+								width="md"
+								name="name"
+								label="用户名"
+								placeholder="请输入用户名"
 							/>
 						</ProForm.Group>
 						<ProForm.Group>
-							<ProFormText width="md" name="email" label="邮箱" placeholder="请输入邮箱" />
+							<ProFormText width="md" name="email" label="邮箱" placeholder="请输入邮箱"/>
 						</ProForm.Group>
 						<ProForm.Group>
-							<ProFormText.Password width="md" name="password" label="密码" type="password" placeholder="请输入密码" />
+							<ProFormText.Password width="md" name="password" label="密码" type="password" placeholder="请输入密码"/>
 						</ProForm.Group>
 					</ModalForm>,
-				]}
+				] }
 			/>
 		</Fragment>
 	);
