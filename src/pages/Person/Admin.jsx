7d7abf7862093724bb89ba8f63fd9fde3ff8a206
自然醒 <2507983704@qq.com>
2023-02-26 11:21:09 +0800

    fix(post): fix the auto brief bug

--- src/pages/Person/Admin.jsx
+++ src/pages/Person/Admin.jsx
@@ -1,15 +1,13 @@
 import { ProTable } from '@ant-design/pro-components';
-import { Button, message, Popconfirm, Form } from 'antd';
-import { EllipsisOutlined, PlusOutlined } from '@ant-design/icons';
+import { Button, message, Popconfirm } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 import React, { Fragment } from 'react';
 import { personApi } from '../../apis/person';
 import {
 	ModalForm,
 	ProForm,
-	ProFormDateRangePicker,
-	ProFormSelect,
 	ProFormText,
-  } from '@ant-design/pro-components';
+} from '@ant-design/pro-components';
 
 const Info = () => {
 	const [ messageApi, contextHolder ] = message.useMessage();
@@ -61,7 +59,7 @@ const Info = () => {
 			width: '12%',
 			title: '操作',
 			valueType: 'option',
-			render: (text, { id,name,email,password }, _, action) => [
+			render: (text, { id, name, email, password }, _, action) => [
 				<Popconfirm
 					key="delete"
 					title="删除管理员"
@@ -83,43 +81,45 @@ const Info = () => {
 						删除
 					</a>
 				</Popconfirm>,
-				<ModalForm key='update'
+				<ModalForm
+					key="update"
 					title="更新管理员"
 					trigger={
 						<a key="update">更新</a>
 					}
-					submitTimeout={2000}
-					onFinish={async (values) => {
-						const { up_id,up_name,up_email,up_password } = values;
-						const { data: { code } } = await personApi.updateAdmin({ up_id,up_name,up_email,up_password });
-						if(code==200){
+					submitTimeout={ 2000 }
+					onFinish={ async (values) => {
+						const { up_id, up_name, up_email, up_password } = values;
+						const { data: { code } } = await personApi.updateAdmin({ up_id, up_name, up_email, up_password });
+						if (code === 200) {
 							message.success('更新成功');
 							action?.reload();
 							return true;
-						}else{
+						} else {
 							message.error('更新失败');
 							return false;
 						}
 
-					}}
-				>
+					} }>
 					<ProForm.Group>
-						<ProFormText width="md" name="up_id" disabled label="编号" initialValue={`${ id }`}/>
+						<ProFormText width="md" name="up_id" disabled label="编号" initialValue={ `${ id }` }/>
 					</ProForm.Group>
 					<ProForm.Group>
 						<ProFormText
-						width="md"
-						name="up_name"
-						label="用户名"
-						placeholder="请输入用户名"
-						initialValue={`${ name }`}
+							width="md"
+							name="up_name"
+							label="用户名"
+							placeholder="请输入用户名"
+							initialValue={ `${ name }` }
 						/>
 					</ProForm.Group>
 					<ProForm.Group>
-						<ProFormText width="md" name="up_email" label="邮箱" placeholder="请输入邮箱" initialValue={`${ email }`}/>
+						<ProFormText width="md" name="up_email" label="邮箱" placeholder="请输入邮箱"
+												 initialValue={ `${ email }` }/>
 					</ProForm.Group>
 					<ProForm.Group>
-						<ProFormText.Password width="md" name="up_password" label="密码" type="password" placeholder="请输入密码" initialValue={`${ password }`}/>
+						<ProFormText.Password width="md" name="up_password" label="密码" type="password" placeholder="请输入密码"
+																	initialValue={ `${ password }` }/>
 					</ProForm.Group>
 				</ModalForm>,
 			],
@@ -127,28 +127,28 @@ const Info = () => {
 	];
 	return (
 		<Fragment>
+			{ contextHolder }
 			<ProTable
 				cardBordered
 				columns={ columns }
-				request = { async (params = {})=>{
-					if(params.id===undefined){
-						const { data:{ code,result } }=await personApi.searchAllAdmin();
-						if(code !== 200){
+				request={ async (params = {}) => {
+					if (params.id === undefined) {
+						const { data: { code, result } } = await personApi.searchAllAdmin();
+						if (code !== 200) {
 							return [];
 						}
 						//获取全部管理员信息
 						let ret = [];
-						for(const item in result[0]){
+						for (const item in result[0]) {
 							ret.push(result[0][item]);
 						}
 						return {
 							data: ret,
 							success: code === 200,
 						};
-					}
-					else{
-						const { data:{ code,result } }=await personApi.searchSuperInfo(params.id,params.name);
-						if(code !== 200){
+					} else {
+						const { data: { code, result } } = await personApi.searchSuperInfo(params.id, params.name);
+						if (code !== 200) {
 							messageApi.open({
 								type: 'error',
 								content: '搜索失败',
@@ -165,49 +165,49 @@ const Info = () => {
 							success: code === 200,
 						};
 					}
-				}}
+				} }
 				rowKey="id"
 				headerTitle="管理员列表"
-				toolBarRender={() => [
-					<ModalForm key='insert'
+				toolBarRender={ () => [
+					<ModalForm
+						key="insert"
 						title="添加管理员"
 						trigger={
 							<Button type="primary">
-							<PlusOutlined />
-							添加
+								<PlusOutlined/>
+								添加
 							</Button>
 						}
-						submitTimeout={2000}
-						onFinish={async (values) => {
-							const { ad_name,ad_email,ad_password } = values;
-							console.log(ad_name,ad_email,ad_password);
-							const { data: { code } } = await personApi.insertAdmin({ ad_name,ad_email,ad_password });
-							if(code==200){
+						submitTimeout={ 2000 }
+						onFinish={ async (values) => {
+							const { ad_name, ad_email, ad_password } = values;
+							const { data: { code } } = await personApi.insertAdmin({ ad_name, ad_email, ad_password });
+							if (code === 200) {
 								message.success('提交成功');
 								return true;
-							}else{
+							} else {
 								message.error('提交失败');
 								return false;
 							}
 
-						}}
-					>
-						 <ProForm.Group>
+						} }>
+						<ProForm.Group>
 							<ProFormText
-							width="md"
-							name="ad_name"
-							label="用户名"
-							placeholder="请输入用户名"
+								width="md"
+								name="ad_name"
+								label="用户名"
+								placeholder="请输入用户名"
 							/>
 						</ProForm.Group>
 						<ProForm.Group>
-							<ProFormText width="md" name="ad_email" label="邮箱" placeholder="请输入邮箱" />
+							<ProFormText width="md" name="ad_email" label="邮箱" placeholder="请输入邮箱"/>
 						</ProForm.Group>
 						<ProForm.Group>
-							<ProFormText.Password width="md" name="ad_password" label="密码" type="password" placeholder="请输入密码" />
+							<ProFormText.Password width="md" name="ad_password" label="密码" type="password"
+																		placeholder="请输入密码"/>
 						</ProForm.Group>
 					</ModalForm>,
-				]}
+				] }
 			/>
 		</Fragment>
 	);
